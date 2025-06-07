a98b09ca1bd26d8e039441b5751dd864e564894b
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-07 17:44:07 +0200

    chart line component + install chart.js

---
+++ src/app/components/charts/chart-line/chart-line.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-line',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-line.component.html',
+  styleUrl: './chart-line.component.scss'
+})
+export class ChartLineComponent {
+
+}
