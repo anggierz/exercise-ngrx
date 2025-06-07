7e1aa4ddf6cdefd30056790f7ee8a592041ccfbe
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-07 17:51:28 +0200

    add line chart component sales profit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,17 @@ import { CounterValueComponent } from './components/counter-value/counter-value.
 import { CounterIncrementComponent } from './components/counter-increment/counter-increment.component';
 import { CounterDecrementComponent } from './components/counter-decrement/counter-decrement.component';
 import { CounterResetComponent } from './components/counter-reset/counter-reset.component';
+import { ChartLineComponent } from './components/charts/chart-line/chart-line.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterValueComponent, CounterIncrementComponent, CounterDecrementComponent, CounterResetComponent],
+  imports: [RouterOutlet,
+    CounterValueComponent,
+    CounterIncrementComponent,
+    CounterDecrementComponent,
+    CounterResetComponent,
+    ChartLineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/charts/chart-line/chart-line.component.ts
+++ src/app/components/charts/chart-line/chart-line.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Chart,
+  registerables
+} from 'chart.js';
 
 @Component({
   selector: 'app-chart-line',
@@ -7,6 +11,38 @@ import { Component } from '@angular/core';
   templateUrl: './chart-line.component.html',
   styleUrl: './chart-line.component.scss'
 })
-export class ChartLineComponent {
+export class ChartLineComponent implements OnInit {
+
+  public chart: any;
+
+  createChart(){
+    this.chart = new Chart("line-chart", {
+      type: 'line',
+      data: {
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12','2022-05-13',
+                 '2022-05-14', '2022-05-15', '2022-05-16','2022-05-17'], 
+        datasets: [
+          {
+            label: "Sales",
+            data: [467, 576, 572, 79, 92, 574, 573, 576],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: [542, 542, 536, 327, 17, 0, 538, 541],
+            backgroundColor: 'limegreen'
+          }  
+        ]
+      },
+      options: {
+        aspectRatio: 2.5
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    Chart.register(...registerables);
+    this.createChart();
+  }
 
 }
